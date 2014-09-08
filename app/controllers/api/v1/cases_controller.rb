84f326b34f023e8783d74b8a1525ae1952f00cb8
Sam Holton <sholton@microsoft.com>
2014-09-08 01:23:48 -0500

    remove unused gems and now unused api endpoint

--- app/controllers/api/v1/cases_controller.rb
+++
@@ -1,8 +0,0 @@
-class Api::V1::CasesController < ApplicationController
-
-	def index
-		response = DESK_CLIENT.cases
-		#render Case.parse_json(response['_embedded']['entries'].first.to_json)
-	end
-
-end
